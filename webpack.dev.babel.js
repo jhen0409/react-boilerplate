695bf08fb09684c75698455bba8a8116ad031657
Jhen <developer@jhen.me>
2016-07-28 18:27:25 +0800

    Add webpack.NamedModulesPlugin for dev

--- webpack.dev.babel.js
+++ webpack.dev.babel.js
@@ -15,6 +15,7 @@ export default {
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
     new webpack.IgnorePlugin(/[^/]+\/[\S]+.prod$/),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
